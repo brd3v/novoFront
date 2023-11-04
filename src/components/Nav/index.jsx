dda4d3a58a98c107b35f1d722ae9b55767b7cca3
brd3v <bnsiq2015@gmail.com>
2023-11-04 15:25:50 -0300

    new front end

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,8 +1,10 @@
+import { FaHouseChimney } from "react-icons/fa6";
 import { Link } from "react-router-dom"
 import Styles from './Nav.module.css'
 const Nav = () => (
     <div className='container'>
       <div className={Styles.nav}>
+      <div className={Styles.item} > <Link className={Styles.inicio} to='/'><FaHouseChimney /></Link></div>
             <div className={Styles.item} > <Link className={Styles.menu}  to='/solteiros'>Solteiros</Link> </div>
             <div className={Styles.item} ><Link className={Styles.menu} to='/casal'> Casal</Link></div>
             <div className={Styles.item} ><Link className={Styles.menu} to='/familia'> familia</Link></div>
